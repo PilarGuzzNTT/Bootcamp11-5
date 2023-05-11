21f26f4fbf82fec7a2de589cd3010bd10038b7d3
PilarGuzzNTT <130760774+PilarGuzzNTT@users.noreply.github.com>
2023-05-11 08:51:47 +0200

    Update App.java

--- prueba1/src/prueba1/App.java
+++ prueba1/src/prueba1/App.java
@@ -8,8 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(18, 6, 4, 15, 55, 78, 12, 9, 8);
 		
-		List<Integer> count = numbers.stream().filter(number ->
-												number >10).collect(Collectors.toList());
+		Long count = numbers.stream().filter(number -> number >10).count();
 		
 		System.out.println(count);
 
